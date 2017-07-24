7a622ad5e2971a48755bb1ca25cad191050958f4
Juan Amari <juan@amari.com.ar>
2017-07-24 20:58:12 -0300

    Added exercise 1.2 (anagrams)

--- src/main/java/ind/juan/learn/chapter/one/exercises/ChapterOneExercises.java
+++ src/main/java/ind/juan/learn/chapter/one/exercises/ChapterOneExercises.java
@@ -1,6 +1,9 @@
 package ind.juan.learn.chapter.one.exercises;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 /**
  * Created by juanamari on 7/18/17.
@@ -42,6 +45,66 @@ public class ChapterOneExercises {
                 return false;
             else
                 wordCount[index] += 1;
+        }
+        return true;
+    }
+
+    /**
+     * Exercise 1.2
+     * Implement an algorithm to check if a string is a permutation of another string.
+     * permutation = anagram
+     * @param input The string to check.
+     * @param target The target string.
+     * @return true if it is a permutation, false if it isn't.
+     */
+    public static boolean isPermutationWithSorting(String input, String target) {
+        if (input == null || target == null || input.length() != target.length()) return false;
+        if (input.length() == 0 && target.length() == 0) return true;
+        if (input.equals(target)) return true;
+
+        // Let's get some character arrays.
+        char[] inputArray = input.toCharArray();
+        char[] targetArray = target.toCharArray();
+        Arrays.sort(inputArray);
+        Arrays.sort(targetArray);
+        return (new String(inputArray)).equals((new String(targetArray)));
+    }
+
+    /**
+     * Exercise 1.2
+     * Implement an algorithm to check if a string is a permutation of another string.
+     * permutation = anagram
+     * HashMap implementation. Longer, but O(n).
+     * @param input The string to check.
+     * @param target The target string.
+     * @return true if it is a permutation, false if it isn't.
+     */
+    public static boolean isPermutationWithMaps(String input, String target) {
+        if (input == null || target == null || input.length() != target.length()) return false;
+        if (input.length() == 0 && target.length() == 0) return true;
+        if (input.equals(target)) return true;
+
+        HashMap<Character, Integer> inputMap = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> targetMap = new HashMap<Character, Integer>();
+
+        for (Character c : input.toCharArray()) {
+            if (inputMap.containsKey(c))
+                inputMap.put(c, inputMap.get(c) + 1);
+            else
+                inputMap.put(c, 0);
+        }
+
+        for (Character c : target.toCharArray()) {
+            if (targetMap.containsKey(c))
+                targetMap.put(c, targetMap.get(c) + 1);
+            else
+                targetMap.put(c, 0);
+        }
+
+        for (Map.Entry<Character, Integer> entry : targetMap.entrySet()) {
+            if (!inputMap.containsKey(entry.getKey())
+                    || inputMap.get(entry.getKey()).intValue() != targetMap.get(entry.getKey()).intValue())
+                return false;
         }
         return true;
     }
--- src/test/java/ind/juan/learn/chapter/one/exercises/ChapterOneExercisesTest.java
+++ src/test/java/ind/juan/learn/chapter/one/exercises/ChapterOneExercisesTest.java
@@ -32,4 +32,59 @@ public class ChapterOneExercisesTest extends TestCase {
         assertTrue(ChapterOneExercises.hasUniqueCharacters(input));
         assertTrue(ChapterOneExercises.hasUniqueCharactersNoAdditionalDataStructures(input));
     }
+
+    /*
+     * The following are tests for Exercise 1.2
+     */
+
+    @Test
+    public void testPermutationsWithNullValuesReturnsFalse() {
+        assertFalse(ChapterOneExercises.isPermutationWithMaps(null, null));
+        assertFalse(ChapterOneExercises.isPermutationWithSorting(null, null));
+        assertFalse(ChapterOneExercises.isPermutationWithMaps(null, "abc"));
+        assertFalse(ChapterOneExercises.isPermutationWithSorting(null, "abc"));
+        assertFalse(ChapterOneExercises.isPermutationWithMaps("abc", null));
+        assertFalse(ChapterOneExercises.isPermutationWithSorting("abc", null));
+    }
+
+    @Test
+    public void testPermutationsWithOneEmptyStringReturnsFalse() {
+        assertFalse(ChapterOneExercises.isPermutationWithMaps("", "a"));
+        assertFalse(ChapterOneExercises.isPermutationWithSorting("", "a"));
+    }
+
+    @Test
+    public void testPermutationsWithDifferentLengthStringsReturnsFalse() {
+        assertFalse(ChapterOneExercises.isPermutationWithMaps("abc", "abcd"));
+        assertFalse(ChapterOneExercises.isPermutationWithSorting("abc", "abcd"));
+    }
+
+    @Test
+    public void testPermutationsWithBothEmptyStringsReturnsTrue() {
+        assertTrue(ChapterOneExercises.isPermutationWithMaps("", ""));
+        assertTrue(ChapterOneExercises.isPermutationWithSorting("", ""));
+    }
+
+    @Test
+    public void testIdenticalStringsReturnsTrue() {
+        assertTrue(ChapterOneExercises.isPermutationWithMaps("abc", "abc"));
+        assertTrue(ChapterOneExercises.isPermutationWithSorting("abc", "abc"));
+    }
+
+    @Test
+    public void testAnagramsReturnTrue() {
+        assertTrue(ChapterOneExercises.isPermutationWithMaps("olleh", "hello"));
+        assertTrue(ChapterOneExercises.isPermutationWithSorting("olleh", "hello"));
+        assertTrue(ChapterOneExercises.isPermutationWithMaps("eybdoog", "goodbye"));
+        assertTrue(ChapterOneExercises.isPermutationWithSorting("eybdoog", "goodbye"));
+    }
+
+    @Test
+    public void testNonAnagramsReturnFalse() {
+        assertFalse(ChapterOneExercises.isPermutationWithMaps("hello", "abcde"));
+        assertFalse(ChapterOneExercises.isPermutationWithSorting("hello", "abcde"));
+        assertFalse(ChapterOneExercises.isPermutationWithMaps("abcd", "abcz"));
+        assertFalse(ChapterOneExercises.isPermutationWithSorting("abcd", "abcz"));
+
+    }
 }
